1f8436e72c3b32a5e81e37c50edc08f9ed02f43a
Elizabeth Teresa <69443070+elizabeth171717@users.noreply.github.com>
2022-03-21 21:58:17 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
 // set date
 const date = document.getElementById("date");
 date.innerHTML = new Date().getFullYear();
+
+var i = 0; // start point
+var images = [];
+ var time = 3000;
+
+ //image list
+  images[0] = '1.jpg';
+  images[1] = '2.jpg';
+  images[2] = '5.jpg';
+  images[3] = '3.jpg';
+  
+
+  function changeImg() {
+      document.slide.src = images[i];
+
+      if(i < images.length - 1) {
+          i++;
+        } else {
+            i = 0;
+        }
+
+        setTimeout("changeImg()", time)
+  }
+
+  window.onload = changeImg;

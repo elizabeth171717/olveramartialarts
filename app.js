b722b6e8aac2f315aa5a7522f1b521d0c1d570d5
Elizabeth Teresa <69443070+elizabeth171717@users.noreply.github.com>
2023-12-06 22:52:06 -0500

    new design

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//gsap animation
+
 gsap.registerPlugin(ScrollTrigger);
 
 let masks = gsap.utils.toArray(".img-mask");
@@ -14,44 +16,26 @@ gsap.to(masks[1], {
   }
 });
 
-let slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("image");
- 
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  
-  slides[slideIndex-1].style.display = "block";  
- 
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
 
 
 
+ScrollTrigger.defaults({
+  toggleActions: "restart pause resume pause"
+});
+
+gsap.to(".heading h1", {
+  scrollTrigger: ".inner-wrapper", 
+  duration: 1, 
+  rotation: 360
+});
 
 
-// Get the button:
-let mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+gsap.to(".left-container", {
+  scrollTrigger: {
+    trigger: ".left-container",
+    toggleActions: "restart pause reverse pause"
+  }, 
+  duration: 2, 
+  backgroundColor: "#000000", 
+  ease: "none"
+});
